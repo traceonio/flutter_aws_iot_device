f4acb49eceedc28bfc32fda11c746add10347880
Dario Coletto <dcoletto.sw@gmail.com>
2020-10-26 23:52:54 +0100

    Update aws_iot_device.dart

--- lib/aws_iot_device.dart
+++ lib/aws_iot_device.dart
@@ -1,4 +1,5 @@
 export './src/device.dart';
 export 'package:tuple/tuple.dart';
 export 'package:mqtt_client/mqtt_client.dart';
+export 'package:mqtt_client/mqtt_server_client.dart';
 export 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
