1570cc966bea1493f70105cd79d4e83e18995641
Dario Coletto <dcoletto.sw@gmail.com>
2020-10-26 23:28:01 +0100

    Using amazon_cognito_identity_dart v2

--- lib/aws_iot_device.dart
+++ lib/aws_iot_device.dart
@@ -1,4 +1,4 @@
 export './src/device.dart';
 export 'package:tuple/tuple.dart';
 export 'package:mqtt_client/mqtt_client.dart';
-export 'package:amazon_cognito_identity_dart/sig_v4.dart';
+export 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
